d4c17fc33d5a9118b334e0aae1fd30e5ded08576
Panagiotis Iatrou <iatrou.panag@gmail.com>
2021-10-02 11:44:46 +0300

    Added game manager

---
+++ Ludum-Dare-49/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Singleton
+    private static GameManager _instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<GameManager>();
+            }
+
+            return _instance;
+        }
+    }
+
+
+}
