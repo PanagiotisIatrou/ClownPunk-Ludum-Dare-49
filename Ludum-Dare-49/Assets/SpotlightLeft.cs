a46465616a138f79f57ddaaa602b0f2c71b6131b
Lionis Emmanouil Georgios (Akis) <akislionis@gmail.com>
2021-10-02 20:53:51 +0200

    spotlight - 4

--- Ludum-Dare-49/Assets/SpotlightLeft.cs
+++ Ludum-Dare-49/Assets/SpotlightLeft.cs
@@ -2,9 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpotilightMovement : MonoBehaviour
+public class SpotlightLeft : MonoBehaviour
 {
-
     //public GameObject light;
     float smooth = 10f;
     float tiltAroundZ = -45f;
@@ -14,11 +13,11 @@ public class SpotilightMovement : MonoBehaviour
     void Update()
     {
         // Smoothly tilts a transform towards a target rotation.
-        if(tiltAroundZ >= targetRotation && targetRotation == -90f)
+        if (tiltAroundZ >= targetRotation && targetRotation == -90f)
         {
             tiltAroundZ -= 0.05f;
         }
-        else if(tiltAroundZ <= targetRotation && targetRotation == 0f)
+        else if (tiltAroundZ <= targetRotation && targetRotation == 0f)
         {
             tiltAroundZ += 0.05f;
         }
@@ -26,7 +25,7 @@ public class SpotilightMovement : MonoBehaviour
         {
             targetRotation = 0f;
         }
-        else if(targetRotation == 0f)
+        else if (targetRotation == 0f)
         {
             targetRotation = -90f;
         }
---
+++ Ludum-Dare-49/Assets/SpotlightRight.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpotlightRight : MonoBehaviour
+{
+    //public GameObject light;
+    float smooth = 10f;
+    float tiltAroundZ = -45f;
+    float targetRotation = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Smoothly tilts a transform towards a target rotation.
+        if (tiltAroundZ >= targetRotation && targetRotation == -90f)
+        {
+            tiltAroundZ -= 0.05f;
+        }
+        else if (tiltAroundZ <= targetRotation && targetRotation == 0f)
+        {
+            tiltAroundZ += 0.05f;
+        }
+        else if (targetRotation == -90f)
+        {
+            targetRotation = 0f;
+        }
+        else if (targetRotation == 0f)
+        {
+            targetRotation = -90f;
+        }
+
+        // Rotate the cube by converting the angles into a quaternion.
+        Quaternion target = Quaternion.Euler(0, 0, tiltAroundZ);
+
+        // Dampen towards the target rotation
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+
+    }
+}
