3d1f9660f56d35da547bc23067f999deec2d9c30
Lionis Emmanouil Georgios (Akis) <akislionis@gmail.com>
2021-10-02 16:21:04 +0200

    The Making of the Buttons

--- Ludum-Dare-49/Assets/Scripts/ButtonListeners.cs
+++ Ludum-Dare-49/Assets/Scripts/ButtonListeners.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class ButtonListeners : MonoBehaviour
 {
-    public void OnPauseButtonListener()
+	public GameObject Play;
+	public GameObject Menu;
+	public GameObject HowToPlay;
+	public GameObject Credits;
+
+
+	public void OnPauseButtonListener()
 	{
 		PauseManager.Pause();
 	}
@@ -13,4 +19,37 @@ public class ButtonListeners : MonoBehaviour
 	{
 		PauseManager.UnPause();
 	}
+
+	public void PlayButton()
+	{
+		Play.SetActive(true);
+		Menu.SetActive(false);
+		HowToPlay.SetActive(false);
+		Credits.SetActive(false);
+	}
+
+	public void MenuButton()
+	{
+		Play.SetActive(false);
+		Menu.SetActive(true);
+		HowToPlay.SetActive(false);
+		Credits.SetActive(false);
+	}
+
+	public void HowToPlayButton()
+    {
+		Play.SetActive(false);
+		Menu.SetActive(false);
+		HowToPlay.SetActive(true);
+		Credits.SetActive(false);
+	}
+
+	public void CreditsButton()
+	{
+		Play.SetActive(false);
+		Menu.SetActive(false);
+		HowToPlay.SetActive(false);
+		Credits.SetActive(true);
+	}
+
 }
