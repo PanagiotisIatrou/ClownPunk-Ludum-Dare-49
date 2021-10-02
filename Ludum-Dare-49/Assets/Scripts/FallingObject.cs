bcbaeaf182a9f83f636afe24de121e96170b3a19
Lionis Emmanouil Georgios (Akis) <akislionis@gmail.com>
2021-10-02 10:56:56 +0200

    Weight system

--- Ludum-Dare-49/Assets/Scripts/FallingObject.cs
+++ Ludum-Dare-49/Assets/Scripts/FallingObject.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class FallingObject : MonoBehaviour
 {
+	public int weight = 0;
+
 	private void Start()
 	{
 		transform.position += Vector3.forward;
+		weight = 1;
 	}
 
 	private void OnCollisionEnter2D(Collision2D collision)
@@ -14,6 +17,14 @@ public class FallingObject : MonoBehaviour
 		if (collision.transform.CompareTag("Bag") || collision.transform.name == "Bot")
 		{
 			gameObject.GetComponent<SpriteFader>().FadeOut();
+			if (collision.gameObject.name == "Bag1")
+            {
+				GameManager.Instance.IncreasePointsLeft(weight);
+			}
+			else
+            {
+				GameManager.Instance.IncreasePointsRight(weight);
+			}
 		}
 	}
 }
--- Ludum-Dare-49/Assets/Scripts/GameManager.cs
+++ Ludum-Dare-49/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
@@ -19,5 +20,38 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    // begin for point changing
+    public TextMeshProUGUI leftText;
+    public TextMeshProUGUI rightText;
+    private int leftWeight;
+    private int rightWeight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        leftWeight = 0;
+        rightWeight = 0;
+    }
 
+    // increase the point of the left bag and check the game over
+    public void IncreasePointsLeft(int theNewPoints)
+    {
+        leftWeight += theNewPoints;
+        leftText.text = leftText.ToString();
+        if (leftWeight >= rightWeight + 3)
+        {
+            Debug.Log("GAME OVER");
+        }
+    }
+
+
+    // increase the point of the right bag and check the game over
+    public void IncreasePointsRight(int theNewPoints)
+    {
+        rightWeight += theNewPoints;
+        rightText.text = rightText.ToString();
+        if (rightWeight >= leftWeight + 3)
+        {
+            Debug.Log("GAME OVER");
+        }
+    }
 }
