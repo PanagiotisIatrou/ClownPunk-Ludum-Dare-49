5c2e62f27804428c4dc1cc510be1e2503f85040b
Panagiotis Iatrou <iatrou.panag@gmail.com>
2021-10-02 22:34:32 +0300

    Trampala sprites

--- Ludum-Dare-49/Assets/Scripts/CustomHinge.cs
+++ Ludum-Dare-49/Assets/Scripts/CustomHinge.cs
@@ -7,6 +7,8 @@ public class CustomHinge : MonoBehaviour
     public GameObject bag1;
     public GameObject bag2;
     public GameObject Hinge;
+    private Vector3 leftBagOffset = new Vector3(-1.05f, 0.3f, -1f);
+    private Vector3 rightBagOffset = new Vector3(1.05f, 0.3f, -1f);
     private float targetBagOffsetY1 = 0f;
     private float targetBagOffsetY2 = 0f;
     private int maxDiff = 3;
@@ -34,8 +36,8 @@ public class CustomHinge : MonoBehaviour
             targetBagOffsetY2 = -(float)diff / maxDiff;
         }
 
-        bag1.transform.position = new Vector2(bag1.transform.position.x, Mathf.Lerp(bag1.transform.position.y, transform.position.y + offsetLengthY * targetBagOffsetY1, 0.05f));
-        bag2.transform.position = new Vector2(bag2.transform.position.x, Mathf.Lerp(bag2.transform.position.y, transform.position.y + offsetLengthY * targetBagOffsetY2, 0.05f));
-        Hinge.transform.rotation = Quaternion.Euler(0f, 0f, Mathf.Atan2((bag2.transform.position.y - transform.position.y) * offsetLengthY, 1.5f) * Mathf.Rad2Deg);
+        bag1.transform.position = new Vector3(bag1.transform.position.x, Mathf.Lerp(bag1.transform.position.y, transform.position.y + leftBagOffset.y + offsetLengthY * targetBagOffsetY1, 0.05f), leftBagOffset.z);
+        bag2.transform.position = new Vector3(bag2.transform.position.x, Mathf.Lerp(bag2.transform.position.y, transform.position.y + rightBagOffset.y + offsetLengthY * targetBagOffsetY2, 0.05f), rightBagOffset.z);
+        Hinge.transform.rotation = Quaternion.Euler(0f, 0f, 4f * Mathf.Atan2((bag2.transform.position.y - transform.position.y - rightBagOffset.y) * offsetLengthY, 1.5f) * Mathf.Rad2Deg);
     }
 }
