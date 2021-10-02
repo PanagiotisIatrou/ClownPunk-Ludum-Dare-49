88d1a61c266d8744e52cfecd431994c4c2d5dd0d
Panagiotis Iatrou <iatrou.panag@gmail.com>
2021-10-02 11:16:24 +0300

    Bag text moves with bags

--- Ludum-Dare-49/Assets/Scripts/BagMovement.cs
+++ Ludum-Dare-49/Assets/Scripts/BagMovement.cs
@@ -1,12 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class BagMovement : MonoBehaviour
 {
+    public TextMeshProUGUI LeftBagText;
+    public TextMeshProUGUI RightBagText;
+    private Camera mainCamera;
     private float speed = 5f;
 
-    private void Update()
+	private void Start()
+	{
+        mainCamera = Camera.main;
+	}
+
+	private void Update()
     {
         Vector2 offset = Vector2.zero;
         if (Input.GetKey(KeyCode.LeftArrow))
@@ -19,5 +28,9 @@ public class BagMovement : MonoBehaviour
         }
 
         transform.position += (Vector3)offset * speed * Time.deltaTime;
+        LeftBagText.transform.position = transform.GetChild(0).position;
+        LeftBagText.transform.position += Vector3.back;
+        RightBagText.transform.position = transform.GetChild(1).position;
+        RightBagText.transform.position += Vector3.back;
     }
 }
